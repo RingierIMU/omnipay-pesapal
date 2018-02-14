610b589d53e0987e2a8905491f0effc9f4e475b3
Gareth Edwards <garrethedwards@gmail.com>
2018-02-14 11:14:41 +0000

    Apply fixes from StyleCI

--- src/OAuth/OAuthToken.php
+++ src/OAuth/OAuthToken.php
@@ -16,7 +16,7 @@ class OAuthToken
 
     /**
      * key = the token
-     * secret = the token secret
+     * secret = the token secret.
      */
     public function __construct(
         string $key,
@@ -28,20 +28,20 @@ class OAuthToken
 
     /**
      * generates the basic string serialization of a token that a server
-     * would respond to request_token and access_token calls with
+     * would respond to request_token and access_token calls with.
      */
     public function to_string() : string
     {
-        return "oauth_token=" .
-               OAuthUtil::urlencode_rfc3986($this->key) .
-               "&oauth_token_secret=" .
+        return 'oauth_token='.
+               OAuthUtil::urlencode_rfc3986($this->key).
+               '&oauth_token_secret='.
                OAuthUtil::urlencode_rfc3986($this->secret);
     }
 
     /**
      * @return string
      */
-    function __toString(): string
+    public function __toString(): string
     {
         return $this->to_string();
     }
